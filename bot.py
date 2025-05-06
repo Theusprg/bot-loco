ad9facd62e829a9ee5cd102e4d23a59857ab8bce
Cia <dragaoking123@gmail.com>
2025-05-06 06:37:38 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,17 @@ from webdriver_manager.chrome import ChromeDriverManager
 from random import uniform, choice
 import threading
 import time
+import os 
+
+def get_chrome_profile_path():
+    username = os.getlogin()
+    base_path = fr"C:\Users\{username}\AppData\Local\Google\Chrome\User Data"
+    return base_path if os.path.exists(base_path) else None
+
+profile_path = get_chrome_profile_path()
+if not profile_path:
+    print("Perfil do navegador não encontrado!")
+    exit()
 
 
 class BotApp:
@@ -23,17 +34,14 @@ class BotApp:
         # Interface
         tk.Label(master, text="Link do site:").pack()
         self.link_entry = tk.Entry(master, width=50)
-        self.link_entry.insert(0, "https://loco.com/streamers/tealz?lang=pt-br")
         self.link_entry.pack()
 
         tk.Label(master, text="Tempo mínimo (s):").pack()
         self.min_time_entry = tk.Entry(master)
-        self.min_time_entry.insert(0, "60")
         self.min_time_entry.pack()
 
         tk.Label(master, text="Tempo máximo (s):").pack()
         self.max_time_entry = tk.Entry(master)
-        self.max_time_entry.insert(0, "90")
         self.max_time_entry.pack()
 
         tk.Button(master, text="Carregar TXT com frases", command=self.carregar_txt).pack(pady=5)
@@ -78,22 +86,17 @@ class BotApp:
     def executar_bot(self):
         self.status.config(text="Status: Inicializando navegador...", fg="green")
 
-        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.6998.165 Safari/537.36"
+
         options = Options()
-        options.add_argument("--disable-blink-features=AutomationControlled")
-        options.add_argument("--disable-infobars")
-        options.add_argument("--disable-extensions")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_argument(f"--user-agent={user_agent}")
+        options.add_argument(f"--user-data-dir={profile_path}")
+        options.add_argument("--profile-directory=Default")
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        options.add_experimental_option("useAutomationExtension", False)
+        options.add_experimental_option('useAutomationExtension', False)
 
         service = Service(ChromeDriverManager().install())
         self.driver = webdriver.Chrome(service=service, options=options)
-    
+
         self.driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
-        self.driver.maximize_window() 
 
         link = self.link_entry.get().strip()
         self.driver.get(link)
